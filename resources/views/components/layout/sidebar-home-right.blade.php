4ea12e922ec2b5d2918d912f676044201d5145f1
thong2085 <129254706+thong2085@users.noreply.github.com>
2023-08-01 23:41:48 +0700

    push backend cho index

--- resources/views/components/layout/sidebar-home-right.blade.php
+++ resources/views/components/layout/sidebar-home-right.blade.php
@@ -59,9 +59,6 @@
     <x-slot name="header">
         <div class="nav" style="border-bottom: 1px solid #3333;">
             <ul class="nav-r" style="width:100%;">
-                <li class="nav-item" style="margin-left: 5px;margin-bottom: 10px;">
-                    <span style="color: #000;font-size: 18px;font-style: normal;font-weight: 700;line-height: normal;">Cộng đồng đặt đơn</span>
-                </li>
                 <x-banner />
             </ul>
 
@@ -141,6 +138,45 @@
                 </div>
             </div> -->
     </x-card>
+    <x-card>
+        <x-slot name="header">
+            <div class="nav">
+                <ul class="nav-r">
+                    <li class="nav-item">
+                        <span style="color: #000;font-size: 18px;font-style: normal;font-weight: 700;line-height: normal;">
+                            <a class="" href="">
+                                {{ ('Tin tức') }}</a></span>
+                    </li>
+                </ul>
+            </div>
+        </x-slot>
+        <x-slot name="footer">
+            <div class="nav">
+          
+                <a href="">
+                    <div>
+                        <div style="display:grid;grid-template-columns:30% 65%;margin-bottom:20px;">
+                            <div class="img-news">
+                                <img src="" />
+                            </div>
+                            <div class="content-news">
+                                <p style="margin-bottom: 20px;"></p>
+                                <div style="display:flex;justify-content:space-between;" class="btn-date-news">
+                                    <span style="color: #A6A4A4;font-size: 10px;font-style: normal;font-weight: 500;line-height: normal;">1 giờ trước</span>
+                                    <a class="text-orange" href="#" style="border-bottom: 1px solid #E0793F;">{{ __('Xem thêm') }}</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+        
+
+            </div>
+            <div class="nav-arrow">
+                <a style="text-align: center;margin:10px 50% 10px 50%" href="#"><img src="{{ asset('viewsCustom/assets/images/down2.png') }}" alt=""></a>
+            </div>
+        </x-slot>
+    </x-card>
     <!-- @if (auth()->check())
     <x-card>
         <x-slot name="header">
@@ -159,6 +195,7 @@
     </x-card>
     @endif -->
 </x-card>
+
 <style>
     * {
         list-style: none;
